9d009dea4264112733c086c50c8c12ad35033690
victor andre bravo burga <victorandrebravoburga@gmail.com>
2017-11-13 13:23:33 -0500

    escena de controlador

---
+++ Assets/Scripts/Controlador/EnemiesSpawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesSpawn : MonoBehaviour {
+
+		public Transform enemy;																	//Prefab del enemigo
+
+
+		
+		void Start()
+		{
+			InvokeRepeating("spawnEnemy",2f, 3f);												//Tiempo de spawn
+
+			for (int i = 0; i < 1; i++){
+				Instantiate(enemy, new Vector3(i * 1f,0,0), Quaternion.identity);				// Instancia 1 copia del prefab del enemigo
+			}
+		}
+
+		void spawnEnemy(){
+			Transform instance = Instantiate(enemy);
+		}
+}
