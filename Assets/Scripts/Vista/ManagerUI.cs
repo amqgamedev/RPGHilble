b6236880e124578c1f4629b57b3e42cedd9094d0
Salvador Otoya <sotoyapici@hotmail.com>
2017-11-14 01:13:03 -0500

    se agrego un folder de scripts llamado vista, en el se creo un script para manejar las escenas, ademas se cambiaron el nombre de estas

---
+++ Assets/Scripts/Vista/ManagerUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class ManagerUI  : MonoBehaviour {
+
+    public void LoadStart()
+    {
+
+        SceneManager.LoadScene("Start");
+
+    }
+
+    public void AppQuit()
+    {
+
+        Application.Quit();
+
+    }
+
+}
