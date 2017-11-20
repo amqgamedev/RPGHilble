a69dd18c903e5a58b828dde903240e34a9c04f35
Alejandro Maticorena <alejexxx123@gmail.com>
2017-11-20 11:05:59 -0500

    se agrego script para crear character

---
+++ Assets/Scripts/Modelo/Character/CreateNewCharacter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateNewCharacter : MonoBehaviour {
+
+	private PlayerBase newPlayer;
+	private bool isMage;
+	private bool isWarrior;
+	// Use this for initialization
+	void Start () {
+		newPlayer = new PlayerBase();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI()
+	{
+		isMage = GUILayout.Toggle(isMage,"Mage Class");
+		isWarrior = GUILayout.Toggle(isWarrior,"WarriorClass");
+		if(GUILayout.Button("Create")){
+			if(isMage){
+			newPlayer.PlayerClass = new MageClass();
+			} else if (isWarrior){
+				newPlayer.PlayerClass = new WarriorClass();
+			}
+			newPlayer.PlayerLevel = 1;
+			newPlayer.Hp = newPlayer.PlayerClass.Hp;
+			newPlayer.Strength = newPlayer.PlayerClass.Strength;
+			newPlayer.Intellect = newPlayer.PlayerClass.Intellect;
+			newPlayer.Speed = newPlayer.PlayerClass.Speed;
+
+			Debug.Log("Player Class =" + newPlayer.PlayerClass.ClassName);
+			Debug.Log("Player level =" + newPlayer.PlayerLevel);
+			Debug.Log("Player HP =" + newPlayer.Hp);
+			Debug.Log("Player Strength =" + newPlayer.Strength);
+			Debug.Log("Player Intellect =" + newPlayer.Intellect);
+			Debug.Log("Player Speed =" + newPlayer.Speed);
+		}
+		
+	}
+}
---
+++ Assets/Scripts/Modelo/Character/PlayerBase.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBase {
+
+	private string playerName;
+	private int playerLevel;
+
+	private Character playerClass;
+
+	//STATS
+    private int hp ;
+    private int strength ;
+    private int intellect ;
+	private int speed;
+	
+	
+	//Get Set
+	public string PlayerName{
+		get{return playerName;}
+		set{playerName = value;}
+	}
+
+	public int PlayerLevel{
+		get{return playerLevel;}
+		set{playerLevel = value;}
+	}
+
+	public Character PlayerClass{
+		get{return playerClass;}
+		set{playerClass = value;}
+	}
+	public int Hp{
+		get{return hp;}
+		set{hp = value;}
+	}
+	public int Strength{
+		get{return strength;}
+		set{strength = value;}
+	}
+
+	public int Intellect{
+		get{return intellect;}
+		set{intellect = value;}
+	}
+
+	public int Speed{
+		get{return speed;}
+		set{speed = value;}
+	}
+	
+}
