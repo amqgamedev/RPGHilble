31549809a77e89c9163d3f6ccc810d40d2e0565f
Salvador Otoya <sotoyapici@hotmail.com>
2017-11-20 13:12:12 -0500

    se creo una escena para el character selection

--- Assets/Scripts/Vista/ManagerUI.cs
+++ Assets/Scripts/Vista/ManagerUI.cs
@@ -10,7 +10,7 @@ public class ManagerUI  : MonoBehaviour {
     public void LoadStart()
     {
 
-        SceneManager.LoadScene("Start");
+        SceneManager.LoadScene("CharSelection");
 
     }
 
