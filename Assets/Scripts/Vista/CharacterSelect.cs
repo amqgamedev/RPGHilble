b02140c9a7af6dc5b1c2a8c1b001ed3ac0df2328
Salvador Otoya <sotoyapici@hotmail.com>
2017-12-03 04:57:41 -0500

    se creo un nuevo script para cambiar de personaje y se borraron los tiles de la escena PlayerGame

---
+++ Assets/Scripts/Vista/CharacterSelect.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelect : MonoBehaviour {
+
+	private GameObject [] characterList;
+	private int index;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+		characterList = new GameObject[transform.childCount];
+
+		//se ponen en el array los modelos
+		for (int i = 0; i < transform.childCount; i++)
+			characterList[i] = transform.GetChild(i).gameObject;
+		
+		//se apagan los renderer
+		foreach(GameObject pj in characterList)
+			pj.SetActive(false);
+
+		if (characterList[0])
+			characterList[0].SetActive(true);
+
+	}
+
+	public void Mage()
+	{
+		//apagar modelo actual
+		characterList[index].SetActive(false);
+
+		index = 1;
+		
+		//prender nuevo modelo
+		characterList[index].SetActive(true);
+	}
+	
+	public void Warrior()
+	{
+		//apagar modelo actual
+		characterList[index].SetActive(false);
+
+		index = 0;
+		
+		//prender nuevo modelo
+		characterList[index].SetActive(true);
+	}
+	
+
+	public void Confirm()
+	{
+		SceneManager.LoadScene("PlayerGame");
+	}
+
+}
