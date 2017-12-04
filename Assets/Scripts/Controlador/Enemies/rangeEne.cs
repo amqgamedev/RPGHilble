0918a8bbd17e73ee784bbf0a56cff7e3340bf92b
victor andre bravo burga <victorandrebravoburga@gmail.com>
2017-12-03 23:58:42 -0500

    enemigo de rango terminado

--- Assets/Scripts/Controlador/Enemies/rangeEne.cs
+++ Assets/Scripts/Controlador/Enemies/rangeEne.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class rangeEne : MonoBehaviour {
 	
-
+	public GameObject bulletPref;
+	public Transform bulletSpawn;
 	public Transform target;            //fija un objetivo
 	private float range = 5f;           // rango del raycast
 	private Vector3 targetDirection;
+	private Vector3 inpDirection;
 
-	// Use this for initialization
+	private float timer = 0;
+	
+	private float rateOfFire = 3f;
+	RaycastHit2D hit;
+	public float bulletSpeed = 5f;
+
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
-	//	InvokeRepeating("FireEne",3.0f,3.0f);
+
+		timer += Time.deltaTime;
+		if(timer > rateOfFire && hit == true){																					
+			GameObject bullet = Instantiate(bulletPref,bulletSpawn.position, bulletSpawn.rotation) as GameObject;			//instanciar la bala
+			bullet.GetComponent<Rigidbody2D>().AddForce( inpDirection.normalized * 5.0f, ForceMode2D.Impulse);				//darle una fuerza
+			bullet.GetComponent<Rigidbody2D>().AddForce(targetDirection.normalized * bulletSpeed, ForceMode2D.Impulse);
+			bullet.GetComponent<BulletScript>().isEnemy = true;																//el prefab de la bala solo choque contra el player
+			timer = 0;
+		}
+	
 
 	FollowP();
 
@@ -26,11 +40,12 @@ public class rangeEne : MonoBehaviour {
 
 		void FollowP () {
 
-       		targetDirection = (target.position - transform.position).normalized * range;                 // direccion perseguir
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, targetDirection, range);           // Vision para que comienze a perseguir
+       		targetDirection = (target.position - transform.position).normalized * range;                 // Direccion perseguir
+            hit = Physics2D.Raycast(transform.position, targetDirection, range);          				 
           
             if (hit.collider != null) {
                 if (hit.collider.CompareTag("Player")) {
+					
                   Debug.Log("++");
                 }
         
